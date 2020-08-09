945d5a367c159ff5a1975bda6e69f86baf8154f9
Braincoke <braincoke@protonmail.com>
2020-08-09 17:39:22 +0200

    Simplify how the cryptopals solution are run

--- README.md
+++ README.md
@@ -9,9 +9,15 @@ New challenges might requires the solutions you built previously.
 For instance the solution for the fourth challenge of Set 1 might rely on the code written for the challenge 2 and 3.
 The file `cryptopals.go` contains the solutions to each challenge, based on the code written in each package.
 
-To run the solution build the program and call it with the set number and challenge number `cryptopals <set_number> <challenge_number>`
+To run the solution build the program and call it with the challenge number `cryptopals <challenge_number>`
 
 ~~~bash
 go build
-./cryptopals 1 4
+./cryptopals 4
+~~~
+
+or
+
+~~~bash
+go run cryptopals 4
 ~~~
--- cryptopals.go
+++ cryptopals.go
@@ -16,128 +16,120 @@ import (
 func main() {
 	// Verify that the user gave a set and a challenge number
 	args := os.Args[1:]
-	if len(args) != 2 {
-		fmt.Println("Please specify the set and the challenge to run: ./cryptopals 1 3 to run the third challenge of the first set")
+	if len(args) != 1 {
+		fmt.Println("Please specify the challenge to run: ./cryptopals 3 to run the third challenge")
 		os.Exit(2)
 	}
 	// Verify that they are integers
-	setToRun, errSet := strconv.ParseInt(args[0], 0, 64)
-	challengeToRun, errChallenge := strconv.ParseInt(args[1], 0, 64)
-	if errSet != nil || errChallenge != nil {
-		fmt.Println("The set and challenge to run must be numbers: ./cryptopals 1 3 to run the third challenge of the first set")
+	challengeToRun, errChallenge := strconv.ParseInt(args[0], 0, 64)
+	if errChallenge != nil {
+		fmt.Println("The set and challenge to run must be numbers: ./cryptopals 3 to run the third challenge")
 		os.Exit(3)
 	}
 
 	// Run the challenge
-	switch setToRun {
 	/**
 	 * Cryptopals set 1
 	 * https://cryptopals.com/sets/1/
 	 */
+	switch challengeToRun {
 	case 1:
-		fmt.Print("### Set 1")
-		switch challengeToRun {
-		case 1:
-			fmt.Println(" - Challenge 1 ###")
-			testString := "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
-			expected := "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
-			base64 := set1.HexToBase64(testString)
-			fmt.Printf("Test string:\t\t%s\n", testString)
-			fmt.Printf("Base64 conversion:\t%s\n", string(base64))
-			fmt.Printf("Expected base64:\t%s\n", expected)
-			fmt.Printf("Result == Expected:\t%t	\n", expected == string(base64))
-		case 2:
-			fmt.Println(" - Challenge 2 ###")
-			var a = "1c0111001f010100061a024b53535009181c"
-			var b = "686974207468652062756c6c277320657965"
-			var expected = "746865206b696420646f6e277420706c6179"
-			ans, _ := set1.XORHex(a, b)
-			fmt.Printf("a:\t\t%s\n", a)
-			fmt.Printf("b:\t\t%s\n", b)
-			fmt.Printf("a ^ b:\t\t%s\n", ans)
-			fmt.Printf("expected:\t%s\n", expected)
-			fmt.Printf("Result == Expected:\t%t\n", expected == ans)
-		case 3:
-			fmt.Println(" - Challenge 3 ###")
-			cipher := "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
-			score, plaintext := set1.SingleByteXorCrack(cipher)
-			fmt.Printf("Cipher:\t\t%s\n", cipher)
-			fmt.Printf("Plaintext:\t%s\n", plaintext)
-			fmt.Printf("Score:\t\t%d\n", score)
-		case 4:
-			fmt.Println(" - Challenge 4 ###")
-			score, cipher, plaintext := set1.DetectSingleCharXOR("set1/challenge4-data.txt")
-			fmt.Printf("Cipher:\t\t%s\n", cipher)
-			fmt.Printf("Plaintext:\t%s", plaintext)
-			fmt.Printf("Score:\t\t%d\n", score)
-		case 5:
-			fmt.Println(" - Challenge 5 ###")
-			key := "ICE"
-			plaintext := "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal"
-			expected := "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"
-			cipher := set1.RepeatingXORString(key, plaintext)
-			fmt.Printf("Plaintext:\t\n----------\n%s\n-----------\n", plaintext)
-			fmt.Printf("Key:\t\t%s\n", key)
-			fmt.Printf("Cipher:\t\t%s\n", cipher)
-			fmt.Printf("Expected:\t%s\n", expected)
-			fmt.Printf("Cipher == Expected:\t%t	\n", expected == cipher)
-		case 6:
-			fmt.Println(" - Challenge 6 ###")
-			filename := "set1/challenge6-data.txt"
-			data, _ := set1.ReadBase64File(filename)
-			key, plaintext := set1.BreakRepeatingKeyXOR(data)
-			fmt.Printf("Key:\t%s\n", string(key))
-			fmt.Println("--------------------------------------------")
-			fmt.Print(string(plaintext))
-		case 7:
-			fmt.Println(" - Challenge 7 ###")
-			Nb := 4
-			// Format the ciphertext as []uint32
-			filename := "set1/challenge7-data.txt"
-			data, _ := set1.ReadBase64File(filename)
-			ciphertext := make([]uint32, len(data)/Nb) // Special case where len(data) divisible by 4
-			for i := 0; i < len(ciphertext); i++ {
-				ciphertext[i] = binary.BigEndian.Uint32([]byte{data[i*Nb], data[i*Nb+1], data[i*Nb+2], data[i*Nb+3]})
+		fmt.Println("### Set 1 - Challenge 1 ###")
+		testString := "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+		expected := "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
+		base64 := set1.HexToBase64(testString)
+		fmt.Printf("Test string:\t\t%s\n", testString)
+		fmt.Printf("Base64 conversion:\t%s\n", string(base64))
+		fmt.Printf("Expected base64:\t%s\n", expected)
+		fmt.Printf("Result == Expected:\t%t	\n", expected == string(base64))
+	case 2:
+		fmt.Println("### Set 1 - Challenge 2 ###")
+		var a = "1c0111001f010100061a024b53535009181c"
+		var b = "686974207468652062756c6c277320657965"
+		var expected = "746865206b696420646f6e277420706c6179"
+		ans, _ := set1.XORHex(a, b)
+		fmt.Printf("a:\t\t%s\n", a)
+		fmt.Printf("b:\t\t%s\n", b)
+		fmt.Printf("a ^ b:\t\t%s\n", ans)
+		fmt.Printf("expected:\t%s\n", expected)
+		fmt.Printf("Result == Expected:\t%t\n", expected == ans)
+	case 3:
+		fmt.Println("### Set 1 - Challenge 3 ###")
+		cipher := "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
+		score, plaintext := set1.SingleByteXorCrack(cipher)
+		fmt.Printf("Cipher:\t\t%s\n", cipher)
+		fmt.Printf("Plaintext:\t%s\n", plaintext)
+		fmt.Printf("Score:\t\t%d\n", score)
+	case 4:
+		fmt.Println("### Set 1 - Challenge 4 ###")
+		score, cipher, plaintext := set1.DetectSingleCharXOR("set1/challenge4-data.txt")
+		fmt.Printf("Cipher:\t\t%s\n", cipher)
+		fmt.Printf("Plaintext:\t%s", plaintext)
+		fmt.Printf("Score:\t\t%d\n", score)
+	case 5:
+		fmt.Println("### Set 1 - Challenge 5 ###")
+		key := "ICE"
+		plaintext := "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal"
+		expected := "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"
+		cipher := set1.RepeatingXORString(key, plaintext)
+		fmt.Printf("Plaintext:\t\n----------\n%s\n-----------\n", plaintext)
+		fmt.Printf("Key:\t\t%s\n", key)
+		fmt.Printf("Cipher:\t\t%s\n", cipher)
+		fmt.Printf("Expected:\t%s\n", expected)
+		fmt.Printf("Cipher == Expected:\t%t	\n", expected == cipher)
+	case 6:
+		fmt.Println("### Set 1 - Challenge 6 ###")
+		filename := "set1/challenge6-data.txt"
+		data, _ := set1.ReadBase64File(filename)
+		key, plaintext := set1.BreakRepeatingKeyXOR(data)
+		fmt.Printf("Key:\t%s\n", string(key))
+		fmt.Println("--------------------------------------------")
+		fmt.Print(string(plaintext))
+	case 7:
+		fmt.Println("### Set 1 - Challenge 7 ###")
+		Nb := 4
+		// Format the ciphertext as []uint32
+		filename := "set1/challenge7-data.txt"
+		data, _ := set1.ReadBase64File(filename)
+		ciphertext := make([]uint32, len(data)/Nb) // Special case where len(data) divisible by 4
+		for i := 0; i < len(ciphertext); i++ {
+			ciphertext[i] = binary.BigEndian.Uint32([]byte{data[i*Nb], data[i*Nb+1], data[i*Nb+2], data[i*Nb+3]})
+		}
+		// Set up the key
+		keyBytes := []byte("YELLOW SUBMARINE")
+		key := make([]uint32, Nb)
+		for i := 0; i < Nb; i++ {
+			key[i] = binary.BigEndian.Uint32([]byte{keyBytes[i*Nb], keyBytes[i*Nb+1], keyBytes[i*Nb+2], keyBytes[i*Nb+3]})
+		}
+		// Decrypt
+		plaintext := set1.ECBDecrypt(ciphertext, key, 4, 10)
+		plaintextBytes := make([]byte, len(plaintext)*Nb)
+		for i := 0; i < len(plaintext); i++ {
+			bytes := make([]byte, Nb)
+			binary.BigEndian.PutUint32(bytes, plaintext[i])
+			for j := 0; j < Nb; j++ {
+				plaintextBytes[i*Nb+j] = bytes[j]
 			}
-			// Set up the key
-			keyBytes := []byte("YELLOW SUBMARINE")
-			key := make([]uint32, Nb)
-			for i := 0; i < Nb; i++ {
-				key[i] = binary.BigEndian.Uint32([]byte{keyBytes[i*Nb], keyBytes[i*Nb+1], keyBytes[i*Nb+2], keyBytes[i*Nb+3]})
+		}
+		fmt.Print(string(plaintextBytes))
+	case 8:
+		fmt.Println("### Set 1 - Challenge 8 ###")
+		lines, _ := set1.ReadFileLines("set1/challenge8-data.txt")
+		detected := 0
+		cipherlines := make([]string, 1)
+		for _, line := range lines {
+			isECB, _ := set1.DetectECB([]byte(line))
+			if isECB {
+				detected++
+				cipherlines = append(cipherlines, line)
 			}
-			// Decrypt
-			plaintext := set1.ECBDecrypt(ciphertext, key, 4, 10)
-			plaintextBytes := make([]byte, len(plaintext)*Nb)
-			for i := 0; i < len(plaintext); i++ {
-				bytes := make([]byte, Nb)
-				binary.BigEndian.PutUint32(bytes, plaintext[i])
-				for j := 0; j < Nb; j++ {
-					plaintextBytes[i*Nb+j] = bytes[j]
-				}
-			}
-			fmt.Print(string(plaintextBytes))
-		case 8:
-			fmt.Println(" - Challenge 8 ###")
-			lines, _ := set1.ReadFileLines("set1/challenge8-data.txt")
-			detected := 0
-			cipherlines := make([]string, 1)
-			for _, line := range lines {
-				isECB, _ := set1.DetectECB([]byte(line))
-				if isECB {
-					detected++
-					cipherlines = append(cipherlines, line)
-				}
-			}
-			fmt.Printf("Detected %d lines using ECB\n", detected)
-			for _, cipher := range cipherlines {
-				fmt.Println(cipher)
-			}
-		default:
-			fmt.Println(" - Unknown challenge number !!!")
-			os.Exit(5)
+		}
+		fmt.Printf("Detected %d lines using ECB\n", detected)
+		for _, cipher := range cipherlines {
+			fmt.Println(cipher)
 		}
 	default:
-		fmt.Println("!!! Unknown Set number !!!")
+		fmt.Println(" - Unknown challenge number !!!")
 		os.Exit(4)
 	}
 	os.Exit(0)
